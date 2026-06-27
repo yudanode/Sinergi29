4859c3ed6e9d44ad45dd215e0d65bd12838652ba
Youpiiiiiiiiiiii <yudannuralif@gmail.com>
2026-06-27 22:01:00 +0700

    feat: implement comments listing and deletion in admin panel

--- app/Http/Controllers/Admin/ComentController.php
+++ app/Http/Controllers/Admin/ComentController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
 
 class ComentController extends Controller
 {
     public function index()
     {
-        return view('admin.comments.index');
+        $comments = Comment::with(['user', 'post'])->latest()->paginate(15);
+        return view('admin.comments.index', compact('comments'));
     }
-    public function destroy($id) {}
+
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+        return back()->with('success', 'Komentar berhasil dihapus.');
+    }
 }
